e9e565d97b44c7169fa69b5828c48937d90ef7ae
Vinmumo <92306415+Vinmumo@users.noreply.github.com>
2024-04-02 08:25:51 +0300

    Update Challenge3.js

--- Challenge3.js
+++ Challenge3.js
@@ -3,3 +3,19 @@ function getArr(){                // prompts user to enter numbers
     const arr1 = arr.split(' ').map(num => parseFloat(num.trim()));
     return arr1            // returns the input numbers in an array
 }
+function prime(num){
+    if (num <= 1) return false;
+    if (num === 2) return true;
+    for (let 1 = 2; i <= Math.sqrt(num); i++){
+        if (num % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+function getPrimes(arr){
+    const arr = getArr();
+    const newArr = arr.filter(num => prime(num));
+    return newArr;
+}
+console.log(getPrimes());
