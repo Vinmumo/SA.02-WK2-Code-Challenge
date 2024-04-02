470e691c9aab92640ce617c767e7c0c1ae73f5ed
Vinmumo <92306415+Vinmumo@users.noreply.github.com>
2024-04-02 08:27:48 +0300

    commit

--- Challenge3.js
+++ Challenge3.js
@@ -3,10 +3,10 @@ function getArr(){                // prompts user to enter numbers
     const arr1 = arr.split(' ').map(num => parseFloat(num.trim()));
     return arr1            // returns the input numbers in an array
 }
-function prime(num){
+function prime(num){      // check if number is a prime number
     if (num <= 1) return false;
     if (num === 2) return true;
-    for (let 1 = 2; i <= Math.sqrt(num); i++){
+    for (let 1 = 2; i <= Math.sqrt(num); i++){    // for loop to check if number is divisible by any number less than its squareroot
         if (num % i === 0){
             return false;
         }
@@ -16,6 +16,6 @@ function prime(num){
 function getPrimes(arr){
     const arr = getArr();
     const newArr = arr.filter(num => prime(num));
-    return newArr;
+    return newArr;        // returns a new array with only prime numbers
 }
 console.log(getPrimes());
